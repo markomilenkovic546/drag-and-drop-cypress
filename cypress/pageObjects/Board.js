4456d36136c8e5978f449c3f06b8c6ef86b52039
markomilenkovic546 <markomilenkovic546@gmail.com>
2024-05-30 18:23:40 +0200

    created Board class

---
+++ cypress/pageObjects/Board.js
@@ -0,0 +1,30 @@
+export default class Board {
+    constructor() {
+        this.column = (columnName) => {
+            return cy.get(`.CycleCard_container__3suZ4:contains(${columnName})`);
+        };
+        this.taskContainer = (columnName) => {
+            return this.column(columnName).find('.TaskContainer_taskContainer__2z1k1');
+        };
+        this.card = (columnName, cardName) => {
+            return this.column(columnName).find(`.Task_task__1btSi:contains(${cardName})`);
+        };
+        this.addItemButton = (columnName) => {
+            return this.column(columnName).find('CycleCard_operationButton__11yd0:contains("Add item")');
+        };
+
+        this.enableEditButton = (columnName) => {
+            return this.column(columnName).find('CycleCard_operationButton__11yd0:contains("Enable edit")');
+        };
+
+        this.addItemInput = (columnName) => {
+            return this.column(columnName).find('.AddTask_addItemInnerContainer__1N_JK');
+        };
+        this.saveItemButton = (columnName) => {
+            return this.column(columnName).find('.AddTask_saveOperation__3nI-D');
+        };
+        this.cancelButton = (columnName) => {
+            return this.column(columnName).find('.AddTask_cancelOperation__3G31s');
+        };
+    }
+}
